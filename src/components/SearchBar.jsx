13693fea242ae8a071e2978d933033f01643f8ca
Asfaltinho <83889522+Asfaltinho@users.noreply.github.com>
2025-12-19 13:05:14 +0100

    fixato dropdown e assegnati id nel componente searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,18 @@
 export default function SearchBar() {
   return (
     <div className="search-row">
-      <input type="text" placeholder="Cerca pianta..." />
-      <button>INVIO</button>
+      <input id="SearchBar" type="text" placeholder="Cerca pianta..." />
 
-      <select>
-        <option value="">filtra famiglia</option>
-        <option>Rosaceae</option>
-        <option>Orchidaceae</option>
-        <option>Asteraceae</option>
-        <option>Fabaceae</option>
-        <option>Poaceae</option>
+      <select id="FamilyDropdwon" defaultValue="">
+        <option disabled hidden value="">Filtra per Famiglia</option>
+        <option value="">Nessun Filtro</option>
+        <option value="Rose">Rosaceae</option>
+        <option value="orchidee">Orchidaceae</option>
+        <option value="Asteracee">Asteraceae</option>
+        <option value="Fabacee">Fabaceae</option>
+        <option value="Poacee">Poaceae</option>
       </select>
+      <button id="SearchBtn">🔍</button>
     </div>
   );
 }
