1e937a4113a9bef6aafe41daedd872400fc7d76b
Asfaltinho <83889522+Asfaltinho@users.noreply.github.com>
2025-12-19 17:25:25 +0100

    sistemato tasto lingue

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 export default function LanguageSwitcher() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="languages">
-      <span>DEU</span>
-      <span>ITA</span>
-      <span>ING</span>
-      <span>JAP 🌐</span>
+    <div className={`language-switcher ${open ? "open" : ""}`}>
+      <div className="globe" onClick={() => setOpen(!open)}>
+        🌐
+      </div>
+
+      <span className="lang top deu">DEU</span>
+      <span className="lang top ita">ITA</span>
+
+      <span className="lang left ing">ING</span>
+      <span className="lang left jap">JAP</span>
     </div>
   );
 }
