22c5537ad1186f072074f82823d127dc11586b3a
Asfaltinho <83889522+Asfaltinho@users.noreply.github.com>
2025-12-20 08:54:56 +0100

    fixato il backend con node.js

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 export default function SearchBar({ onSearch }) {
   const [query, setQuery] = useState("");
-  const [family, setFamily] = useState("");
 
   return (
     <div className="search-row">
@@ -13,19 +12,7 @@ export default function SearchBar({ onSearch }) {
         onChange={(e) => setQuery(e.target.value)}
       />
 
-      <select
-        value={family}
-        onChange={(e) => setFamily(e.target.value)}
-      >
-        <option value="">Nessun filtro</option>
-        <option value="Rosaceae">Rosaceae</option>
-        <option value="Orchidaceae">Orchidaceae</option>
-        <option value="Asteraceae">Asteraceae</option>
-        <option value="Fabaceae">Fabaceae</option>
-        <option value="Poaceae">Poaceae</option>
-      </select>
-
-      <button onClick={() => onSearch(query, family)}>🔍</button>
+      <button onClick={() => onSearch(query)}>🔍 →</button>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,41 @@
-import { useState } from "react";
 import "../styles/home.css";
 import bg from "../assets/bg.jpeg";
+import { useState } from "react";
 
-import SearchBar from "../components/SearchBar";
-import PlantCard from "../components/PlantCard";
-import InfoBox from "../components/InfoBox";
-import SideLogo from "../components/SideLogo";
+import SearchBar from "../components/SearchBar.jsx";
+import PlantCard from "../components/PlantCard.jsx";
+import InfoBox from "../components/InfoBox.jsx";
+import SideLogo from "../components/SideLogo.jsx";
 
 export default function Home() {
   const [plants, setPlants] = useState([]);
 
-  async function fetchPlants(query, family) {
-    let url = `https://trefle.io/api/v1/plants/search?token=usr-pHbEB8YkhsmkSbvfI1YHrmXqoVJqSkYDV1gGE3O2asM&q=${query}`;
-
-    if (family) {
-      url += `&filter[family]=${family}`;
-    }
-
-    const res = await fetch(url);
-    const data = await res.json();
-    setPlants(data.data || []);
+  async function fetchPlants(query) {
+    const response = await fetch(`http://localhost:3000/plants?q=${query}`);
+    const data = await response.json();
+    setPlants(data.data);
   }
 
   return (
     <div className="home" style={{ backgroundImage: `url(${bg})` }}>
       <div className="overlay">
-
         <div className="content">
           <h1>SCEGLI LA PIANTA DI CUI SAPERE LE INFORMAZIONI</h1>
 
           <SearchBar onSearch={fetchPlants} />
 
-          <div className="results">
-            {plants.map((plant) => (
-              <PlantCard
-                key={plant.id}
-                name={plant.scientific_name}
-                image={plant.image_url}
-              />
-            ))}
+          <div className="main-box">
+            <div className="results">
+              {plants.map((plant) => (
+                <PlantCard
+                  key={plant.id}
+                  name={plant.scientific_name}
+                  image={plant.image_url}
+                />
+              ))}
+            </div>
+
+            <InfoBox />
           </div>
         </div>
 
