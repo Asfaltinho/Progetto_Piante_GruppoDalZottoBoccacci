c8bae0c4dd02bae02f003b187b0ca0076a08c0ca
Asfaltinho <83889522+Asfaltinho@users.noreply.github.com>
2025-12-20 09:46:47 +0100

    Revert "Reapply "Merge branch 'main' of https://github.com/Asfaltinho/Progetto_Piante_GruppoDalZottoBoccacci""
    This reverts commit 0d407c6a5e7e2b1155858ebd003bac4f829e5ba9.

--- src/components/SideLogo.jsx
+++ src/components/SideLogo.jsx
@@ -3,8 +3,6 @@ import '../styles/home.css'
 export default function SideLogo() {
   return (
     <div className="side-logo">
-      <img src={Logo} alt="logo" />
-      <LanguageSwitcher />
         <h2>
           <img src={Logo} width="100%"></img>
         </h2>
